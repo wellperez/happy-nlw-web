c145444a5f62c055a0b8aeb81676236be42dca7b
Wellington Perez <wellingtonaperez@gmail.com>
2020-10-15 09:13:52 -0300

    feat: add popup icon

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet'
+import { FiArrowRight, FiPlus } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import Leaflet from 'leaflet'
 
 import 'leaflet/dist/leaflet.css'
 
-import mapMarketImg from '../images/map-marker.svg'
+import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58,68],
+  iconAnchor: [29,68],
+  popupAnchor: [170, 2]
+})
 
 function OrphanagesMap() {
   return (
     <div id="page-map">
       <aside>
         <header>
-          <img src={mapMarketImg} alt="Happy"/>
+          <img src={mapMarkerImg} alt="Happy"/>
 
           <h2>Escolha um orfanato no mapa</h2>
           <p>Muitas crianças estão esperando a sua visita :)</p>
@@ -33,7 +42,20 @@ function OrphanagesMap() {
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-      </Map> 
+      
+        <Marker
+          icon = { mapIcon }
+          position={[-21.1487928,-49.0110665]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Lar das meninas
+            <Link to="" >
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>               
+          </Popup>   
+        </Marker> 
+             
+      </Map>       
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff"/>
